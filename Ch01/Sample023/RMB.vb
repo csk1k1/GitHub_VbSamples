928cadac561dcf4593bda369071eaf5c84746282
WILI Lee <cs_wxd@hotmail.com>
2018-06-28 07:56:49 +0800

    0230

--- Ch01/Sample023/RMB.vb
+++ Ch01/Sample023/RMB.vb
@@ -8,8 +8,7 @@
             RMBChinese = "需转换的金额整数长度超过了12位！"
             Exit Function
         End If
-        'strRmbExp = "仟佰拾亿仟佰拾万仟佰拾元角分"
-        'Debug.Print(StrReverse(strRmbExp))
+
         strRmbExp = "分角元拾佰仟万拾佰仟亿拾佰仟"
         strRmbDa = "零壹贰叁肆伍陆柒捌玖"
         intN = 0
@@ -24,14 +23,13 @@
         strResult = ""
         intCount = Len(strNum)
 
-
         For i = 1 To intCount
-            strResult = Mid(strRmbDa, Val(Mid(strNum, i, 1)) + 1, 1)
+            Dim j = CInt(Mid(strNum, i, 1))
+            strResult = Mid(strRmbDa, j + 1, 1) + IIf(j = 0 AndAlso (i <> 7 OrElse i <> 11), "", Mid(strRmbExp, i, 1)) + strResult
         Next
 
-        'For i = 1 To intCount
-        '    strResult = Mid(strRmbDa, Val(Mid(strNum, intCount - i + 1, 1)) + 1, 1) + IIf(Mid(strRmbExp, i, 1) = "元", Mid(strRmbExp, i, 1) + " ", Mid(strRmbExp, i, 1)) + strResult
-        'Next
+        If 
+
 
         RMBChinese = IIf(intN = 1, "负" + strResult, strResult)
 
